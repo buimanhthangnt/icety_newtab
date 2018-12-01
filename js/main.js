603f928bacf51156dd3f89a1ce8ed3883852ef95
thangbm <buimanhthangnt@gmail.com>
2018-12-01 21:58:35 +0700

    add fancy things to todo list

--- js/main.js
+++ js/main.js
@@ -97,6 +97,7 @@ $(document).ready(function($) {
 				return t;
 			}
 		})
+		congratulate()
 		setNotitext();
 		update(todos);
 	}
@@ -107,11 +108,11 @@ $(document).ready(function($) {
 		});
 		for (let i = 0; i < todos.length; i++) {
 			let todo = todos[i];
-			let title = todo.title.length > 30 ? todo.title.substring(0, 30) + "..." : todo.title;
+			let title = todo.title;
 			let date = todo.date;
 			let time = todo.time;
 			let tag = todo.done ? "Completed" : "Todo";
-			let strEle = `<div id=\"todo-${i}\" class=\"row\"><div class=\"col-md-2\"></div><div class=\"col-md-8\"><div class=\"job-post-item bg-white p-4 d-block d-md-flex align-items-center\"><div class=\"mb-4 mb-md-0 mr-5\"><div class=\"job-post-item-header d-flex align-items-center\"><h2 class=\"mr-3 text-black h4\">${title}</h2><div class=\"badge-wrap\"><span class=\"${todo.done ? "bg-success" : "bg-warning"} text-white badge py-2 px-4\">${tag}</span></div></div><div class=\"job-post-item-body d-block d-md-flex\"><div class=\"mr-5\"><span class=\"fl-bigmug-line-portfolio23\"></span> ${date}</div><div><span class=\"fl-bigmug-line-big104\"></span> <span>${time}</span></div></div></div><div class=\"ml-auto\"><span id="todo-delete-${i}" class=\"btn btn-secondary rounded-circle btn-favorite text-gray-500\" style=\"margin-right: 30px !important; color: white !important; border-color: #f23a2e; background: #f23a2e;\"><span class=\"icon-trash\"></span></span><button class=\"btn btn-primary py-2\" ${todo.done ? "disabled" : ""} id="todo-done-${i}">Done</button></div></div></div><div class=\"col-md-2\"></div></div>`;
+			let strEle = `<div id=\"todo-${i}\" class=\"row\"><div class=\"col-md-2\"></div><div class=\"col-md-8\"><div class=\"job-post-item bg-white p-4 d-block d-md-flex align-items-center\"><div class=\"mb-4 mb-md-0 mr-5\" style=\"flex: 1; overflow: hidden\"><div class=\"job-post-item-header d-flex align-items-center\"><h2 class=\"mr-3 text-black h4\">${title}</h2><div class=\"badge-wrap\"><span class=\"${todo.done ? "bg-success" : "bg-warning"} text-white badge\" style=\"padding: 6px 12px !important\">${tag}</span></div></div><div class=\"job-post-item-body d-block d-md-flex\"><div class=\"mr-5\"><span class=\"fl-bigmug-line-portfolio23\"></span> ${date}</div><div><span class=\"fl-bigmug-line-big104\"></span> <span>${time}</span></div></div></div><div class=\"ml-auto\"><span id="todo-delete-${i}" class=\"btn btn-secondary rounded-circle btn-favorite text-gray-500\" style=\"margin-right: 30px !important; color: white !important; border-color: #f23a2e; background: #f23a2e;\"><span class=\"icon-trash\"></span></span><button class=\"btn btn-primary py-2\" ${todo.done ? "disabled" : ""} id="todo-done-${i}">Done</button></div></div></div><div class=\"col-md-2\"></div></div>`;
 			$("#todoslist").append($(strEle));
 		}
 		for (let i = 0; i < todos.length; i++) {
@@ -191,4 +192,96 @@ $(document).ready(function($) {
 		$("#text-area").addClass(`text-${textColor}`);
 		alert("Changed text color to " + textColor);
 	});
+
+
+	let particles = ['.blob', '.star'],
+	 $congratsSection = $('#congrats'),
+	 $title = $('#title');
+
+
+	// Congratulation
+	// $congratsSection.click(fancyPopIn);
+	init({
+		numberOfStars: 20,
+		numberOfBlobs: 8
+	});
+	function congratulate() {
+		let congrats = ['well done!', 'good job!', 'keep going!', 'impressive!', 'excellent!', 'awesome!', 'great!']
+		let rand_int = Math.floor(Math.random() * congrats.length);
+		$("#congrats").css('visibility', 'visible').css('top', `calc(40% + ${$(window).scrollTop()}px)`);
+		$("#title_congrat").text(congrats[rand_int]);
+		fancyPopIn();
+		setTimeout(function () {
+			$("#congrats").css('visibility', 'hidden');
+		}, 1000);
+	}
+
+	function fancyPopIn() {
+		reset();
+		animateText();
+		
+		for (let i = 0, l = particles.length; i < l; i++) {
+			animateParticles(particles[i]);
+		}
+	}
+
+	function animateText() {
+		TweenMax.from($title, 0.65, {
+			scale: 0.4,
+			opacity: 0,
+			rotation: 15,
+			ease: Back.easeOut.config(5),
+		});
+	}
+
+	function animateParticles(selector) {
+		let xSeed = _.random(350, 380);
+		let ySeed = _.random(120, 170);
+		
+		$.each($(selector), function(i) {
+			let $particle = $(this);
+			let speed = _.random(1, 4);
+			let rotation = _.random(20, 100);
+			let scale = _.random(0.8, 1.5);
+			let x = _.random(-xSeed, xSeed);
+			let y = _.random(-ySeed, ySeed);
+
+			TweenMax.to($particle, speed, {
+				x: x,
+				y: y,
+				ease: Power1.easeOut,
+				opacity: 0,
+				rotation: rotation,
+				scale: scale,
+				onStartParams: [$particle],
+				onStart: function($element) {
+					$element.css('display', 'block');
+				},
+				onCompleteParams: [$particle],
+				onComplete: function($element) {
+					$element.css('display', 'none');
+				}
+			});
+		});
+	}
+
+	function reset() {
+		for (let i = 0, l = particles.length; i < l; i++) {
+			$.each($(particles[i]), function() {
+				TweenMax.set($(this), { x: 0, y: 0, opacity: 1 });
+			});
+		}
+		
+		TweenMax.set($title, { scale: 1, opacity: 1, rotation: 0 });
+	}
+
+	function init(properties) {
+		for (let i = 0; i < properties.numberOfStars; i++) {
+		$congratsSection.append('<div class="particle star fa fa-star ' + i + '"></div>');
+		}
+		
+		for (let i = 0; i < properties.numberOfBlobs; i++) {
+		$congratsSection.append('<div class="particle blob ' + i + '"></div>');
+		}	
+	}
 });
